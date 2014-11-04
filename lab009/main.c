b70b6eba226d8d1723408d5f2f3dd0266461e83a
watremtezdamien <damien0390@gmail.com>
2014-11-04 13:44:19 +0100

    rate monotonic
    mise en place des trois tache necessare avec trois fonction

--- lab009/main.c
+++ lab009/main.c
@@ -6,8 +6,12 @@
 #define GPIO0_DIR            				(*(unsigned long*)0x2009C000)
 #define GPIO0_SET            				(*(unsigned long*)0x2009C018)
 #define GPIO0_CLR            				(*(unsigned long*)0x2009C01C)
+#define mainDELAY_LOOP_COUNT		(0xfffff)
 
 void vTaskFunction( void *pvParameters );
+void vTaskFunction2( void *pvParameters );
+void vTaskFunction3( void *pvParameters );
+
 void vOrdonnanceurIn(int iPin);
 void vOrdonnanceurOut(int iPin);
 
@@ -16,13 +20,15 @@ unsigned long ulTaskNumber[configEXPECTED_NO_RUNNING_TASKS];
 unsigned long ultimeIdle;
 const char *pcTextForTask1 = "Task 1 is running\n";
 const char *pcTextForTask2 = "Task 2 is running\n";
+const char *pcTextForTask3 = "Task 3 is running\n";
+
 
 int main(void)
 {
   xTaskCreate( vTaskFunction, "Task 1", 240, (void*)pcTextForTask1, 1, NULL );
 
-  xTaskCreate( vTaskFunction, "Task 2", 240, (void*)pcTextForTask2, 2, NULL );
-
+  xTaskCreate( vTaskFunction2, "Task 2", 240, (void*)pcTextForTask2, 2, NULL );
+	xTaskCreate( vTaskFunction3, "Task 2", 240, (void*)pcTextForTask3, 2, NULL );
   vTaskStartScheduler();	
 	
   for(;;);
@@ -34,6 +40,7 @@ int main(void)
 */
 void vTaskFunction(void *pvParameters)
 {
+	volatile unsigned long ul;
   char *pcTaskName;
   portTickType xLastWakeTime;
 
@@ -44,7 +51,45 @@ void vTaskFunction(void *pvParameters)
   for(;;)	{
     vPrintString( pcTaskName );
 
-    vTaskDelayUntil( &xLastWakeTime, ( 250 / portTICK_RATE_MS ) );
+    for(ul = 0; ul < mainDELAY_LOOP_COUNT; ul++) {
+
+    }
+  }
+}
+void vTaskFunction2(void *pvParameters)
+{
+	volatile unsigned long ul;
+  char *pcTaskName;
+  portTickType xLastWakeTime;
+
+  pcTaskName = ( char * ) pvParameters;
+
+  xLastWakeTime = xTaskGetTickCount();
+
+  for(;;)	{
+    vPrintString( pcTaskName );
+
+    for(ul = 0; ul < mainDELAY_LOOP_COUNT; ul++) {
+
+    }
+  }
+}
+void vTaskFunction3(void *pvParameters)
+{
+	volatile unsigned long ul;
+  char *pcTaskName;
+  portTickType xLastWakeTime;
+
+  pcTaskName = ( char * ) pvParameters;
+
+  xLastWakeTime = xTaskGetTickCount();
+
+  for(;;)	{
+    vPrintString( pcTaskName );
+
+    for(ul = 0; ul < mainDELAY_LOOP_COUNT; ul++) {
+
+    }
   }
 }
 /**
