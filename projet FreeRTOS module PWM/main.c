59688293761d32461361a60bf3d45ffd446c3cbf
watremtezdamien <damien0390@gmail.com>
2014-11-16 23:15:02 +0100

    ajout de code
    debut de code pour la partie PWM
    avec commentaire

--- projet FreeRTOS module PWM/main.c
+++ projet FreeRTOS module PWM/main.c
@@ -5,6 +5,8 @@
   
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
+#include "user.h"
 //#include "basic_io.h"
 
 #include "ARMCM3.h"
@@ -12,10 +14,35 @@
 
 unsigned long ulTaskNumber[configEXPECTED_NO_RUNNING_TASKS];
 
+/* declaration fonction */
+static void vPwmTaskReceiver(void* pvParameters);
  /*declarantion xqueuehandle*/
-//xQueueHandle xPwmValueQueue;
+xQueueHandle xPwmValueQueue;
+xPwmData_t xPwmData ={50,1000} ;
 int main (void)
 {
-	
-	for( ;; );
+	xPwmValueQueue = xQueueCreate(2,sizeof(xPwmData_t)); // creation de la file de messsage pour la tache PWM
+	if( xPwmValueQueue != NULL )
+	{
+	xTaskCreate(vPwmTaskReceiver,"PWM",240,NULL,1,NULL);
+	}
+		for( ;; );
 }
+
+
+
+/**
+@brief : definition de la tache PWM 
+@details : cette tache generer un signal PWM via une tache simple la file de message devras comporté deux valeurs 
+					 la frequence et le cycle c'est cette tache qui seras l'action de la sortie 
+*/
+static void vPwmTaskReceiver(void* pvParameters)
+{
+	unsigned char cCmpt=0;
+	xPwmData_t xPwmDataReceive;
+	for ( ;; )
+	{
+		
+	}
+}
+
---
+++ projet FreeRTOS module PWM/user.h
@@ -0,0 +1,22 @@
+#ifndef USER_H
+#define USER_H
+typedef struct xPwmData
+{
+	unsigned int Cycle;
+	unsigned long frequency;
+}xPwmData_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*USER_H*/
