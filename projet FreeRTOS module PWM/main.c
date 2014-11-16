89e38e73c8bb88ba9a2f1ee527775c5cc353b62a
watremtezdamien <damien0390@gmail.com>
2014-11-16 22:45:51 +0100

    creation du projet PWM

---
+++ projet FreeRTOS module PWM/main.c
@@ -0,0 +1,21 @@
+/**
+@ author : Watremetz Damien 
+@ detail : application PWM 
+*/
+  
+#include "FreeRTOS.h"
+#include "task.h"
+//#include "basic_io.h"
+
+#include "ARMCM3.h"
+/*declaration variable global*/
+
+unsigned long ulTaskNumber[configEXPECTED_NO_RUNNING_TASKS];
+
+ /*declarantion xqueuehandle*/
+//xQueueHandle xPwmValueQueue;
+int main (void)
+{
+	
+	for( ;; );
+}
