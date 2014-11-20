ddb90265084a34f9c7f038972f1fb70b8e19ce23
watremtezdamien <damien0390@gmail.com>
2014-11-20 18:59:03 +0100

    ajout type def struct pour la queue uart
    ajout type def pour file de message UART

--- projet FreeRTOS module I2C/user.h
+++ projet FreeRTOS module I2C/user.h
@@ -1,13 +1,24 @@
 #ifndef USER_H
 #define USER_H
+#include <stdint.h>
 
+/*structure communication I2C transmition data en Queue*/
 typedef struct xI2cDataTransmit
 {
-	unsigned char cAddress;
-	unsigned char cData;
+	uint8_t cSlaveAddress;
+	const uint8_t *Data;
+	int iTxSize;
+
 	
 }xI2cDataTransmit_t;
 
+/*structure communication UART reception et reception UART */
+
+typedef struct xUartDataReceive
+{
+	
+	uint8_t uiI2cTransmit; 
+}xUartDataReceive_t;
 
 
 
